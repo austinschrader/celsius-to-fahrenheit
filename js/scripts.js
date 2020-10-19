cc1be1ad7a2ca8d1efa60bb459d641234b8056e1
Austin Schrader <austin.jschrader@gmail.com>
2020-10-19 13:29:10 -0700

    Finished the function to convert celsius to fahrenheit
    Co-authored-by: Eric Endsley <eric.endsley4@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+function convertCelsiusToFahrenheit(number1) {
+  return alert((promptResult * 1.8) + 32)
+};
+
+const promptResult = parseInt(prompt("Enter in what you want to convert to Fahrenheit"))
+convertCelsiusToFahrenheit(0)
